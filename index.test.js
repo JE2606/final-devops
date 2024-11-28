e7acc5c9a4e537afab40c21bdcebe94e7a89ec23
JE2606 <juan.elias0626@gmail.com>
2024-11-28 14:06:07 -0400

    arreglando error del test

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 test('Página contiene "¡Hola Mundo!"', () => {
-    const indexPath = path.join(__dirname, '../index.html');
+    const indexPath = path.join(__dirname, 'index.html');
     const htmlContent = fs.readFileSync(indexPath, 'utf-8');
-    expect(htmlContent).toContain('<h1>¡Hola Mundo!</h1>');
+    expect(htmlContent).toContain('<h1>¡Hola Mundo!, soy Juan Hernandez 2023-0613</h1>');
 });
